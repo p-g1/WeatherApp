81705f456ecd4439fb03989718d7526f77606943
Pete <petergarvin1@gmail.com>
2018-09-24 23:20:11 +0100

    updating api call to use fetch

--- weather/script.js
+++ weather/script.js
@@ -1,11 +1,9 @@
-const http = new XMLHttpRequest(),
-      url='https://api.openweathermap.org/data/2.5/forecast?q=London,GB&units=metric&appid=c73ba9db4e80a01ed143a90d6a017168';
+const url='https://api.openweathermap.org/data/2.5/forecast?q=London,GB&units=metric&appid=c73ba9db4e80a01ed143a90d6a017168';
 
-http.open('GET', url, true);
-http.send();
-
-http.onreadystatechange = function() {
-  var data = JSON.parse(http.response);
+fetch(url).then(function(response) {
+              return response.json();
+            })
+          .then(function(json) {
 
   //capture date
   var date = new Date(); 
@@ -19,7 +17,7 @@ http.onreadystatechange = function() {
                        'Clear': {'background': '../background/Clear.jpg', 'icon': 'far fa-sun'},
                        'Clouds' : {'background': '../background/Clouds.jpg', 'icon': 'fas fa-cloud'}};
 
-  document.body.style.backgroundImage = 'url("' + images[data.list[0].weather[0].main].background; + '")';
+  document.body.style.backgroundImage = 'url("' + images[json.list[0].weather[0].main].background; + '")';
 
   //populate 'focus' details (main upper section of page)
   const details = ['focus-type', 'focus-temp', 'focus-pic-container', 'location'];
@@ -27,11 +25,11 @@ http.onreadystatechange = function() {
   //iterate over each and attach relevant data
   details.forEach(e => {
     document.getElementById(e).innerHTML =
-      e === 'focus-type' ? data.list[0].weather[0].main:
+      e === 'focus-type' ? json.list[0].weather[0].main:
       //multiplication and division to achieve 2 decimal places
-      e === 'focus-temp' ? Math.round(data.list[0].main.temp * 10 ) / 10 + " o".sup() + "C":
-      e === 'location' ? data.city.name + ', ' + data.city.country:
-      '<i class="' + images[data.list[0].weather[0].main].icon + ' fa-10x focus-img"></i>';
+      e === 'focus-temp' ? Math.round(json.list[0].main.temp * 10 ) / 10 + " o".sup() + "C":
+      e === 'location' ? json.city.name + ', ' + json.city.country:
+      '<i class="' + images[json.list[0].weather[0].main].icon + ' fa-10x focus-img"></i>';
   })
  
   //populate 5 day forecast data
@@ -50,11 +48,11 @@ http.onreadystatechange = function() {
                 const id = x.slice(0, x.length-1); 
                 document.getElementById(x).innerHTML = 
                 //index*8 used to get one regular daily reading. API provides 3 hourly (24/3 = 8)
-                  id === 'type' ? data.list[i*8].weather[0].main:
-                  id === 'pic' ? '<i class="' + images[data.list[i*8].weather[0].main].icon + ' fa-3x"></i>':
+                  id === 'type' ? json.list[i*8].weather[0].main:
+                  id === 'pic' ? '<i class="' + images[json.list[i*8].weather[0].main].icon + ' fa-3x"></i>':
                   id === 'date' ? date.toLocaleString('en-gb').slice(0, 5):
                   //multiplication and division to achieve 2 decimal places
-                  Math.round(data.list[i*8].main.temp * 10 ) / 10 + " o".sup() + "C";
+                  Math.round(json.list[i*8].main.temp * 10 ) / 10 + " o".sup() + "C";
               });
   });  
-}
+});
