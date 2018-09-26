c9ea8ec8687063d0786cc450afb2ec78462af3dc
Pete <petergarvin1@gmail.com>
2018-09-26 10:32:12 +0100

    reverting api call from fetch to xmlhttp to accomdate IE11

--- weather/scripts/main.js
+++ weather/scripts/main.js
@@ -1,24 +1,25 @@
-import * as api from './api.js';
 import * as focusWeather from './focus-weather.js';
 import * as forecast from './forecast.js';
 import * as images from './images.js';
 
 //get api data
-api.getData().then(api.handleErrors)
-             .then(response => {
-                return response.json();
-            })
-            .then(json => {  
-              const currentWeather = json.list[0].weather[0].main;
-              var date = new Date();
-            
-              //set long hand date  
-              document.getElementById('dateToday').innerHTML = date.toLocaleString('en-gb', {weekday: 'long', day: 'numeric', month: 'long'});
-            
-              //set main background and icon from json
-              document.body.style.backgroundImage = 'url("' + images.pictures[currentWeather].background; + '")';
-           
-              focusWeather.populateFocusWeather(json, images.pictures, currentWeather);
+const http = new XMLHttpRequest();
+const url='https://api.openweathermap.org/data/2.5/forecast?q=London,GB&units=metric&appid=c73ba9db4e80a01ed143a90d6a017168';
 
-              forecast.populateFiveDay(json, images.pictures);
-            });
+http.open('GET', url, true);
+http.send();
+http.onreadystatechange = function() {
+  var json = JSON.parse(http.response);         
+  const currentWeather = json.list[0].weather[0].main;
+  var date = new Date();
+            
+  //set long hand date  
+  document.getElementById('dateToday').innerHTML = date.toLocaleString('en-gb', {weekday: 'long', day: 'numeric', month: 'long'});
+            
+  //set main background and icon from json
+  document.body.style.backgroundImage = 'url("' + images.pictures[currentWeather].background; + '")';
+           
+  focusWeather.populateFocusWeather(json, images.pictures, currentWeather);
+
+  forecast.populateFiveDay(json, images.pictures);
+  };
